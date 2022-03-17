a867d9976982be181ad1af3131692e8801350dc8
prasann3a <prasanna6796@gmail.com>
2022-03-17 08:03:46 +0530

    variables and expressions

---
+++ src/main/scala/lectures/part1basics/Expressions.scala
@@ -0,0 +1,18 @@
+package lectures.part1basics
+
+object Expressions extends App{
+  val x = 1+2
+  println(x )
+
+//  Instructions vsExpressions(VALUE)
+  // if expression
+
+  val aCondition = true
+  val aConditionedValue = if(aCondition) 5 else 3
+//  if is an expression not a condition
+  println(aConditionedValue)
+
+  // EVERYTHIING IN SCALA IS AN EXPRESSIN
+
+
+}
